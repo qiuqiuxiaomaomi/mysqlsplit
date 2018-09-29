fdb501b9cf51615cc06b6b51fbcf9eb4f1400265
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-09-29 16:49:45 +0800

    MQ消息实现事务

--- src/main/java/com/splittree/service/RocketMqTransactionService.java
+++ src/main/java/com/splittree/service/RocketMqTransactionService.java
@@ -4,8 +4,7 @@ import org.springframework.stereotype.Service;
 
 /**
  * Created by yangmingquan on 2018/9/29.
- * TCC 分布式事务
  */
 @Service
-public class MySqlTccTransactionService {
+public class RocketMqTransactionService {
 }
